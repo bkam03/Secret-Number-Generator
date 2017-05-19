fc70f3d2a3ab445f08a6c0a617729076df2e3594
BK <bkam03@yahoo.com>
2017-05-18 18:58:31 -1000

    function is now in the right place, 2 tests passed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
+//Create a module that returns a function that will return a random number.
+//work on the secret_number.js file.
 'use strict';
+
 module.exports = function() {
-  
+  function randomNumberGen(){
+      var randomNumber = Math.floor(Math.random() * 1000001);
+      return randomNumber;
+    }
+    return randomNumberGen;
 };
